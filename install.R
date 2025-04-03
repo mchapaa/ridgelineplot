ae79b0007eb129b94f96bdaa3ff17ccb3e63f2f5
mchapaa <mchapaa@uoc.edu>
2025-04-03 22:47:57 +0200

    Update install.R

--- install.R
+++ install.R
@@ -1,3 +1,10 @@
-install.packages(c("readxl", "dplyr", "lubridate", "ggplot2", "ggridges"))
 install.packages("IRkernel")
 IRkernel::installspec(user = FALSE)
+
+install.packages(c(
+  "readxl", 
+  "dplyr", 
+  "lubridate", 
+  "ggplot2", 
+  "ggridges"
+))
